09a58d6d237b315f6c73ea0fb323e892b758f2c9
acer <rabindra.tamang@g-mana.com>
2021-03-16 18:23:30 +0545

    Add user join message

--- public/script.js
+++ public/script.js
@@ -1,87 +1,30 @@
 
-const muteUnmute = () => {
-  const enabled = myVideoStream.getAudioTracks()[0].enabled;
-  if (enabled) {
-    myVideoStream.getAudioTracks()[0].enabled = false;
-    setUnmuteButton();
-  } else {
-    setMuteButton();
-    myVideoStream.getAudioTracks()[0].enabled = true;
-  }
-};
+const socket = io("/");
+const videoGrid = document.querySelector("#video-grid");
+const peer = new Peer(undefined, {
+  path: "/peerjs",
+  host: "/",
+  port: "443",
+  config: {
+    iceServers: [
+      {
+        urls: "turn:13.234.239.38?transport=tcp",
+        username: "user",
+        credential: "root",
+      },
+      { urls: "stun:stun.l.google.com:19302" },
+    ],
+  },
+});
 
-const playStop = () => {
-  let enabled = myVideoStream.getVideoTracks()[0].enabled;
-  if (enabled) {
-    myVideoStream.getVideoTracks()[0].enabled = false;
-    setPlayVideo();
-  } else {
-    setStopVideo();
-    myVideoStream.getVideoTracks()[0].enabled = true;
-  }
-};
-
-const setMuteButton = () => {
-  const html = `
-    <i class="fas fa-microphone"></i>
-    <span>Mute</span>
-  `;
-  document.querySelector(".main__mute_button").innerHTML = html;
-};
-
-const setUnmuteButton = () => {
-  const html = `
-    <i class="unmute fas fa-microphone-slash"></i>
-    <span>Unmute</span>
-  `;
-  document.querySelector(".main__mute_button").innerHTML = html;
-};
-
-const setStopVideo = () => {
-  const html = `
-    <i class="fas fa-video"></i>
-    <span>Stop Video</span>
-  `;
-  document.querySelector(".main__video_button").innerHTML = html;
-};
-
-const setPlayVideo = () => {
-  const html = `
-  <i class="stop fas fa-video-slash"></i>
-    <span>Play Video</span>
-  `;
-  document.querySelector(".main__video_button").innerHTML = html;
-};
-
-const leaveMeeting = () => {
-  window.existingCall.close();
-};
-
+let myVideoStream;
+const myVideo = document.createElement("video");
+myVideo.muted = true;
 
 const username = prompt("Please enter your name");
 
 if (username) {
-  const socket = io("/");
-  const videoGrid = document.querySelector("#video-grid");
-  const peer = new Peer(undefined, {
-    path: "/peerjs",
-    host: "/",
-    port: "443",
-    config: {
-      iceServers: [
-        {
-          urls: "turn:13.234.239.38?transport=tcp",
-          username: "user",
-          credential: "root",
-        },
-        { urls: "stun:stun.l.google.com:19302" },
-      ],
-    },
-  });
-
-  let myVideoStream;
-  const myVideo = document.createElement("video");
-  myVideo.muted = true;
+  
   navigator.mediaDevices
     .getUserMedia({
       video: true,
@@ -158,4 +101,63 @@ if (username) {
     chatWindow.scrollTop(chatWindow.prop("scrollHeight"));
   };
 }
+
+
+const muteUnmute = () => {
+  const enabled = myVideoStream.getAudioTracks()[0].enabled;
+  if (enabled) {
+    myVideoStream.getAudioTracks()[0].enabled = false;
+    setUnmuteButton();
+  } else {
+    setMuteButton();
+    myVideoStream.getAudioTracks()[0].enabled = true;
+  }
+};
+
+const playStop = () => {
+  let enabled = myVideoStream.getVideoTracks()[0].enabled;
+  if (enabled) {
+    myVideoStream.getVideoTracks()[0].enabled = false;
+    setPlayVideo();
+  } else {
+    setStopVideo();
+    myVideoStream.getVideoTracks()[0].enabled = true;
+  }
+};
+
+const setMuteButton = () => {
+  const html = `
+    <i class="fas fa-microphone"></i>
+    <span>Mute</span>
+  `;
+  document.querySelector(".main__mute_button").innerHTML = html;
+};
+
+const setUnmuteButton = () => {
+  const html = `
+    <i class="unmute fas fa-microphone-slash"></i>
+    <span>Unmute</span>
+  `;
+  document.querySelector(".main__mute_button").innerHTML = html;
+};
+
+const setStopVideo = () => {
+  const html = `
+    <i class="fas fa-video"></i>
+    <span>Stop Video</span>
+  `;
+  document.querySelector(".main__video_button").innerHTML = html;
+};
+
+const setPlayVideo = () => {
+  const html = `
+  <i class="stop fas fa-video-slash"></i>
+    <span>Play Video</span>
+  `;
+  document.querySelector(".main__video_button").innerHTML = html;
+};
+
+const leaveMeeting = () => {
+  window.existingCall.close();
+};
 
